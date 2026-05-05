d6076dc97fc029ff1d295b941c43ec04c4dcde6b
97928175 <25111412g@connect.polyu.hk>
2026-05-06 02:39:33 +0800

    update

--- Assets/Scripts/StartDialogueOnClick.cs
+++ Assets/Scripts/StartDialogueOnClick.cs
@@ -1,15 +1,42 @@
 using UnityEngine;
+using UnityEngine.UI;
+using PixelCrushers.DialogueSystem;
 
 public class StartDialogueOnClick : MonoBehaviour
 {
     public DialogueChatBridge dialogueChatBridge;
     public string conversationName = "ScamChatExample";
 
+    private Button button;
+    private bool hasStarted = false;
+
+    void Awake()
+    {
+        button = GetComponent<Button>();
+    }
+
     public void StartChat()
     {
+        if (hasStarted) return;
+
+        hasStarted = true;
+
+        if (button != null)
+            button.interactable = false;
+
+        // 如果之前还有残留对话，先停掉
+        if (DialogueManager.isConversationActive)
+        {
+            DialogueManager.StopConversation();
+        }
+
         if (dialogueChatBridge != null)
         {
             dialogueChatBridge.OpenChatAndStartConversation(conversationName);
+        }
+        else
+        {
+            Debug.LogError("DialogueChatBridge 没有绑定！");
         }
     }
 }
